b7ceca894461773484a3accb728fd0c272ef5551
kurth4cker <kurth4cker@gmail.com>
2025-05-20 11:32:38 +0300

    sb: implement sb_concat()

--- example.c
+++ example.c
@@ -9,13 +9,33 @@
 
 int main(void)
 {
-    Sb sb = { 0 };
-    sb_append_str(&sb, "hello");
-    sb_append_char(&sb, ' ');
-    sb_append_str(&sb, "world");
+    {
+        Sb sb = { 0 };
+        sb_append_str(&sb, "hello");
+        sb_append_char(&sb, ' ');
+        sb_append_str(&sb, "world");
 
-    char *str = sb_cstr(&sb);
-    assert(strcmp(str, "hello world") == 0);
-    free(str);
-    sb_destroy(&sb);
+        char *str = sb_cstr(&sb);
+        assert(strcmp(str, "hello world") == 0);
+        free(str);
+        sb_destroy(&sb);
+    }
+    {
+        Sb hello = { 0 };
+        sb_append_str(&hello, "hello");
+        Sb world = { 0 };
+        sb_append_str(&world, "world");
+
+        Sb sb = { 0 };
+        sb_concat(&sb, &hello);
+        sb_append_char(&sb, ' ');
+        sb_concat(&sb, &world);
+
+        char *str = sb_cstr(&sb);
+        assert(strcmp(str, "hello world") == 0);
+        free(str);
+        sb_destroy(&sb);
+        sb_destroy(&hello);
+        sb_destroy(&world);
+    }
 }
